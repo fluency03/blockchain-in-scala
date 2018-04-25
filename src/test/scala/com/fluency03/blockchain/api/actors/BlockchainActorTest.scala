ec142453fb2f472975227a008ac2661412723bb4
fluency03 <fluency.03@gmail.com>
2018-04-25 16:08:24 +0100

    add BlockchainActorTest

--- src/test/scala/com/fluency03/blockchain/api/actors/BlockchainActorTest.scala
+++ src/test/scala/com/fluency03/blockchain/api/actors/BlockchainActorTest.scala
@@ -1,5 +1,48 @@
 package com.fluency03.blockchain.api.actors
 
-class BlockchainActorTest {
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{DefaultTimeout, ImplicitSender, TestKit}
+import com.fluency03.blockchain.api.{FailureMsg, SuccessMsg}
+import com.fluency03.blockchain.api.actors.BlockchainActor.{CreateBlockchain, DeleteBlockchain, GetBlockchain}
+import com.fluency03.blockchain.core.Blockchain
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.concurrent.duration._
+
+class BlockchainActorTest extends TestKit(ActorSystem("BlockchainActorTest")) with DefaultTimeout with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll  {
+
+  override def afterAll: Unit = {
+    shutdown()
+  }
+
+  val blockchainActor: ActorRef = system.actorOf(Props[BlockchainActor])
+
+  "A BlockchainActor" should {
+    "Respond with a Blockchain." in {
+      blockchainActor ! GetBlockchain
+      expectMsg(None)
+
+      within(15 seconds) {
+        blockchainActor ! CreateBlockchain
+        expectMsgType[SuccessMsg]
+      }
+
+      blockchainActor ! CreateBlockchain
+      expectMsgType[FailureMsg]
+
+      blockchainActor ! GetBlockchain
+      expectMsgType[Some[Blockchain]]
+
+      blockchainActor ! DeleteBlockchain
+      expectMsg(SuccessMsg(s"Blockchain deleted."))
+
+      blockchainActor ! DeleteBlockchain
+      expectMsg(FailureMsg(s"Blockchain does not exist."))
+
+      blockchainActor ! "other"
+      expectNoMessage
+    }
+  }
 
 }
