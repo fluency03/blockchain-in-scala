331186d38dfd83fc34d1fbc6cc81247003bc0e7d
fluency03 <fluency.03@gmail.com>
2018-05-12 21:22:56 +0200

    fix SerdeTest

--- src/test/scala/com/github/fluency03/blockchain/misc/SerdeTest.scala
+++ src/test/scala/com/github/fluency03/blockchain/misc/SerdeTest.scala
@@ -2,7 +2,7 @@ package com.github.fluency03.blockchain.misc
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class SerdeTest extends FlatSpec with Matchers with {
+class SerdeTest extends FlatSpec with Matchers {
 
 
 
